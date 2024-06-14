a5eeb377ef72bb2a88d0a48eafb492fbf41e3e91
Lorrin <363819948@qq.com>
2024-06-14 10:56:02 +0800

    原目录结构图片压缩后保持目录结构保存到新目录

--- image.js
+++ image.js
@@ -2,14 +2,14 @@
  * @Author: Lorrin
  * @Date: 2024-06-09 15:02:49
  * @LastEditors: Lorrin
- * @LastEditTime: 2024-06-12 15:08:32
+ * @LastEditTime: 2024-06-14 10:47:08
  * @Description: 
  */
 const fs = require('fs').promises;
 const path = require('path');
 const sharp = require('sharp');
 
-async function resizeAndSaveImage(inputPath, outputPath, maxSizeKB) {
+async function resizeAndSaveImage(inputPath, outPath, dirName, maxSizeKB) {
   const maxSizeBytes = maxSizeKB * 1024;
   let quality = 80; // 初始质量
   let resizedWidth = null;
@@ -29,8 +29,15 @@ async function resizeAndSaveImage(inputPath, outputPath, maxSizeKB) {
       .toBuffer();
 
     if (buffer.length <= maxSizeBytes) {
+      console.log('start save new image');
       // 如果图片大小满足要求，保存图片
-      await fs.writeFile(outputPath, buffer);
+      try {
+        await fs.mkdir(dirName, { recursive: true });
+        await fs.writeFile(outPath, buffer);
+        console.log('文件已成功写入:', outPath);
+      } catch (error) {
+        console.error('写入文件时发生错误:', err);
+      }
     } else if (quality > 10) {
       // 如果图片太大，降低质量并递归尝试
       quality -= 10;
@@ -45,15 +52,23 @@ async function resizeAndSaveImage(inputPath, outputPath, maxSizeKB) {
   await adjustSize();
 }
 
-async function processImagesInDir(inPath, outPath, maxSizeKB) {
-  const files = await fs.readdir(inPath);
+// async function processImagesInDir(inPath, outPath, maxSizeKB) {
+//   const files = await fs.readdir(inPath);
+async function processImagesInDir(filePathList, outPath, maxSizeKB) {
+  const files = [...filePathList]
   const imageFiles = files.filter(file => path.extname(file).toLowerCase() === '.jpg');
 
   for (const imageFile of imageFiles) {
-    const inputPath = path.join(inPath, imageFile);
-    const outputPath = path.join(outPath, imageFile);
+    const inputPath = imageFile;
+    const imageName = path.basename(imageFile); // 文件名
+    const dirName = path.dirname(imageFile).replace("inputImages", "outImages"); // 文件所在目录
+    console.log('2-dirName:', dirName);
+    // console.log('imageName: ', imageName);
+    // const outputPath = path.join(outPath, imageName); // 转换后保存到新目录下
+    const outputPath =  imageFile.replace("inputImages", "outImages")// 保存原目录结构
+    console.log('3-outputPath:',outputPath);
     try {
-      await resizeAndSaveImage(inputPath, outputPath, maxSizeKB);
+      await resizeAndSaveImage(inputPath, outputPath, dirName, maxSizeKB);
       console.log(`Processed ${imageFile}`);
     } catch (error) {
       console.error(`Error processing ${imageFile}:`, error);
@@ -61,10 +76,40 @@ async function processImagesInDir(inPath, outPath, maxSizeKB) {
   }
 }
 
+// await resizeAndSaveImage(inputPath, outputPath, maxSizeKB)
+//               console.log(`Processed ${imageFile}`);
 // 使用示例
 const dirPath = './inputImages'; // 图片所在的文件夹路径
 const outPath = './outImages'; // 压缩后的文件夹路径
 const maxSizeKB = 200; // 最大图片大小（KB）
-processImagesInDir(dirPath, outPath, maxSizeKB)
-  .then(() => console.log('All images processed successfully.'))
-  .catch(error => console.error('Error processing images:', error));
+var imagesPathList = [];
+async function traverseDirectory(dir) {
+  // 读取目录内容
+  const files = await fs.readdir(dir);
+
+  files.forEach(file => {
+    const fullPath = path.join(dir, file);
+    fs.stat(fullPath).then(stats => {
+      // console.log(stats);
+      // 如果是目录，则递归遍历
+      if (stats.isDirectory()) {
+        console.log(`遍历目录: ${fullPath}`);
+        traverseDirectory(fullPath);
+      } else {
+        // 如果是文件，则输出文件名
+        console.log(`找到文件: ${fullPath}`);
+        // 保存读取的图片路径
+        imagesPathList.push(fullPath);
+      }
+    })
+  });
+}
+traverseDirectory(dirPath)
+console.log('1----', imagesPathList);
+
+setTimeout(() => {
+  console.log('2----', imagesPathList);
+  processImagesInDir(imagesPathList, outPath, maxSizeKB)
+    .then(() => console.log('All images processed successfully.'))
+    .catch(error => console.error('Error processing images:', error));
+}, 2000)
